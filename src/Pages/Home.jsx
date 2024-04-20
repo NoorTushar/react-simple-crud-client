67b6bdd3262d1723c18ef368b0005c477ddc9c11
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-20 18:27:37 +0600

    sending data to express server

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -10,6 +10,17 @@ const Home = () => {
 
       const user = { name, email };
       console.log(user);
+
+      fetch("http://localhost:3000/users", {
+         method: "POST",
+         headers: {
+            "Content-Type": "application/json",
+         },
+         body: JSON.stringify(user),
+      })
+         .then((res) => res.json())
+         .then((data) => console.log(data))
+         .catch((err) => console.log(err));
    };
 
    return (
