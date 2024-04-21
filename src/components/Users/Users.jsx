093334f8df6bb82976ffdb83154f4f22723bacaa
NoorTushar <tushar.noor.khan@gmail.com>
2024-04-21 10:31:00 +0600

    deleted user from database

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,7 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { json, useLoaderData } from "react-router-dom";
 
 const Users = () => {
    const users = useLoaderData();
+
+   const handleDeleteUser = (id) => {
+      console.log(id);
+      fetch(`http://localhost:3000/users/${id}`, {
+         method: "DELETE",
+      })
+         .then((res) => res.json())
+         .then((data) => {
+            console.log(data);
+            if (data.deletedCount > 0) {
+               alert("Data removed from database");
+            }
+         })
+         .catch((err) => console.error(err));
+   };
+
    return (
       <div className="text-center py-[40px]">
          <h3>Total Users: {users.length}</h3>
@@ -18,7 +34,14 @@ const Users = () => {
                         </h2>
                         <p>{user.email}</p>
                         <div className="card-actions justify-end">
-                           <button className="btn btn-primary">Delete</button>
+                           <button
+                              onClick={() => {
+                                 handleDeleteUser(user._id);
+                              }}
+                              className="btn btn-primary"
+                           >
+                              Delete
+                           </button>
                         </div>
                      </div>
                   </div>
